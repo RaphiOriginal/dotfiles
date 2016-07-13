c220e5df0a3dc3c9e5fdc3f950983673173ec939
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-07-13 15:36:48 +0200

    added fish config to git

---
+++ config.fish
@@ -0,0 +1,7 @@
+eval sh $HOME/.config/base16-shell/base16-eighties.dark.sh
+set -Ux fish_term256
+
+# Load fishmarks (http://github.com/techwizrd/fishmarks)
+. $HOME/.fishmarks/marks.fish
+
+alias yolo "python ~/Documents/Projects/fun/yolo.py"
